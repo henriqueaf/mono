3eaa2f7b77fbe263be92a9d04e7d11fdd4a52702
Henrique Desktop <henrique_af1@msn.com>
2013-08-02 15:41:04 -0300

    Commit inicial do projeto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mono::Application.config.secret_token = '291d2b77fe821a3e0f76e6ebdfbd4f0e46dfbcdfeb06ac4d66c654730d44dd2ef16e4148c9866527985b2a654166553e189c61b800fc8eb6856bb2db85cd1e5a'
